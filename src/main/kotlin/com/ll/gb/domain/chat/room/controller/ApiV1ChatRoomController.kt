3f42490e8bfe249a6a72290113e40714a440551b
jhs512 <jangka512@gmail.com>
2024-12-13 00:26:57 +0900

    GET /api/v1/chat/rooms

--- src/main/kotlin/com/ll/gb/domain/chat/room/controller/ApiV1ChatRoomController.kt
+++ src/main/kotlin/com/ll/gb/domain/chat/room/controller/ApiV1ChatRoomController.kt
@@ -1,21 +1,20 @@
 package com.ll.gb.domain.chat.room.controller
 
 import com.ll.gb.domain.chat.room.dto.ChatRoomDto
+import com.ll.gb.domain.chat.room.service.ChatRoomService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/chat/rooms")
-class ApiV1ChatRoomController {
+class ApiV1ChatRoomController(
+    private val chatRoomService: ChatRoomService
+) {
     @GetMapping
     fun getItems(): List<ChatRoomDto> {
-        return listOf(
-            ChatRoomDto(1, "room1"),
-            ChatRoomDto(2, "room2"),
-            ChatRoomDto(3, "room3"),
-            ChatRoomDto(4, "room4"),
-            ChatRoomDto(5, "room5"),
-        )
+        return chatRoomService
+            .findAll()
+            .map { ChatRoomDto(it) }
     }
 }
--- src/main/kotlin/com/ll/gb/domain/chat/room/dto/ChatRoomDto.kt
+++ src/main/kotlin/com/ll/gb/domain/chat/room/dto/ChatRoomDto.kt
@@ -1,8 +1,18 @@
 package com.ll.gb.domain.chat.room.dto
 
-data class ChatRoomDto(
-    val id: Int,
-    val name: String
-) {
+import com.ll.gb.domain.chat.room.entity.ChatRoom
+import java.time.LocalDateTime
 
+data class ChatRoomDto(
+    val id: Long,
+    val createdAt: LocalDateTime,
+    val modifiedAt: LocalDateTime,
+    val title: String
+) {
+    constructor(chatRoom: ChatRoom) : this(
+        id = chatRoom.id,
+        createdAt = chatRoom.createdAt,
+        modifiedAt = chatRoom.modifiedAt,
+        title = chatRoom.title
+    )
 }
