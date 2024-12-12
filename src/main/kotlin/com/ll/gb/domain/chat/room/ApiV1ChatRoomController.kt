3b33c56213b48918805475646899df425e47f3b3
jhs512 <jangka512@gmail.com>
2024-12-13 00:06:38 +0900

    work

---
+++ src/main/kotlin/com/ll/gb/domain/chat/room/ApiV1ChatRoomController.kt
@@ -0,0 +1,21 @@
+package com.ll.gb.domain.chat.room
+
+import com.ll.gb.domain.chat.room.dto.ChatRoomDto
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/chat/rooms")
+class ApiV1ChatRoomController {
+    @GetMapping
+    fun getItems(): List<ChatRoomDto> {
+        return listOf(
+            ChatRoomDto(1, "room1"),
+            ChatRoomDto(2, "room2"),
+            ChatRoomDto(3, "room3"),
+            ChatRoomDto(4, "room4"),
+            ChatRoomDto(5, "room5"),
+        )
+    }
+}
---
+++ src/main/kotlin/com/ll/gb/domain/chat/room/dto/ChatRoomDto.kt
@@ -0,0 +1,8 @@
+package com.ll.gb.domain.chat.room.dto
+
+data class ChatRoomDto(
+    val id: Int,
+    val name: String
+) {
+
+}
